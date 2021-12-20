2bf6dd3cd89fbf35746f4614b81e16e69e1a4c99
Amon <anriarudoragon@gmail.com>
2021-12-20 23:56:01 +0200

    config comments

--- Bot/config/client.py
+++ Bot/config/client.py
@@ -1,3 +1,8 @@
+# Your Discord ID
 owner_id = 261205439121719298
+
+# Bot Commands Prefix
 command_prefix = "."
+
+# Bot Embeds Color
 color = 0x00FFFF
--- Bot/config/logging.py
+++ Bot/config/logging.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 
+# Logging File Place & Name
 log_file = "Bot/Logs/{0}.log".format(datetime.now().strftime("%m%d%Y-%H%M%S"))
+
+# Logging File Encoding
 log_encoding = "utf-8"
+
+# Logging Records Format
 log_format = "[%(asctime)s] %(levelname)s : %(message)s"
+
+# Logging Date Format for Records
 log_dateformat = "%m/%d/%Y %I:%M:%S %p"
